315dd6fe1ec1818a78091dccba006f6a19010301
David Buck <david.buck@datascience.fun>
2019-04-17 09:41:41 +0100

    Completed Scikit supervised learning course Learn Machine Learning Workshop 4 notes

---
+++ src/Scikit-learn intro.py
@@ -0,0 +1,17 @@
+plt.figure()
+sns.countplot(x='education', hue='party', data=partylist, palette='RdBu')
+plt.xticks([0, 1], ['No', 'Yes'])
+plt.show()
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    # the fit method returns the object self, so we can instantiate 
+    # and fit in one line 
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.
+    4 )
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{} neighbor(s)".format(n_neighbors))
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+    axes[0].legend(loc=3)
